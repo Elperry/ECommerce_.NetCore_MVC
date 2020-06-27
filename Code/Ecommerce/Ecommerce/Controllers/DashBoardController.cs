8402966475309ea5d48c387ae073f677f6389cd6
Mokhtar Elsmahy <mokhtarelsmahy80@gmail.com>
2020-06-28 00:05:38 +0200

    Layout and Distribution

---
+++ Code/Ecommerce/Ecommerce/Controllers/DashBoardController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class DashBoardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Index33()
+        {
+            return View();
+        }
+    }
+}
