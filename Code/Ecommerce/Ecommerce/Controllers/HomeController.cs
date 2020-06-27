9946bb5dbf6fe02da977b272da8e6b758f31b7a5
Mokhtar Elsmahy <mokhtarelsmahy80@gmail.com>
2020-06-27 22:59:18 +0200

    Home Theme Added

--- Code/Ecommerce/Ecommerce/Controllers/HomeController.cs
+++ Code/Ecommerce/Ecommerce/Controllers/HomeController.cs
@@ -23,6 +23,17 @@ namespace Ecommerce.Controllers
             return View();
         }
 
+
+        public IActionResult Shop()
+        {
+            return View();       
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
