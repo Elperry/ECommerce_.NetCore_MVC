69f638e1b6f05e3e87d3c3cd622648a4fd8412c1
mohamed <mohammedafifi153@gmail.com>
2020-07-01 17:05:03 +0200

    shopping cart

---
+++ Code/Ecommerce/Ecommerce/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Ecommerce.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Ecommerce.Controllers
+{
+    public class ProductController : Controller
+    {
+        ApplicationDbContext db;
+        public ProductController(ApplicationDbContext _db)
+        {
+            db = _db;
+        }
+        public IActionResult PrdDetails(int id)
+        {
+            if (id == null)
+                return RedirectToAction("Error", "Home");
+
+            // var product = db.Products.Find(id);
+            var product = db.Products.Include(p => p.Category).FirstOrDefault(p => p.ProductId == id);
+            const int numOfRelated = 4;
+            if (product is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            else
+            {
+                var relatedProducts = db.Products.OrderByDescending(p => p.ProductId).Take(4)
+                    .Where(p => p.CategoryId == product.CategoryId);
+                ViewBag.product = product;
+                int size = relatedProducts.Count();
+                if (size < numOfRelated)
+                {
+                    for (int i = 0; i < numOfRelated - size; i++)
+                    {
+                        relatedProducts.ToList().Add(product);
+                    }
+                }
+                return View(relatedProducts);
+            }
+        }
+    }
+}
