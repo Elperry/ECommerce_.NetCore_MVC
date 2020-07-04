2660452355bbbe611aa905d4319e04f5b290af49
mohamed <mohammedafifi153@gmail.com>
2020-07-05 00:39:03 +0200

    shopping cart latest change

---
+++ Code/Ecommerce/Ecommerce/Components/CartViewComponent.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ecommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Components
+{
+    public class CartViewComponent:ViewComponent
+    {
+        private readonly ApplicationDbContext db;
+        CheckoutViewModel model;
+
+        public CartViewComponent(ApplicationDbContext db)
+        {
+            this.db = db;
+            model = new CheckoutViewModel();
+        }
+        public IViewComponentResult Invoke()
+        {
+            var cartProductCookie = Request.Cookies["CartProducts"];
+            int quantity = 0;
+            if (cartProductCookie != null && cartProductCookie != "")
+            {
+                model.CartProductIDs = cartProductCookie.Split('-').Select(x => int.Parse(x)).ToList();
+                model.CartProducts = db.Products.Include(ww => ww.Offer).Where(p => model.CartProductIDs.Contains(p.ProductId)).ToList();
+                foreach (var product in model.CartProducts)
+                {
+                    quantity = model.CartProductIDs.Count(x => x == product.ProductId);
+                    if (product.ProductUnitInStock != 0)
+                        model?.Order?.Add(product.ProductId, quantity);
+                }
+                TempData["name"] = "from shopping cart";
+
+            }
+            return View(model);
+        }
+    }
+}
